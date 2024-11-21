2ca4c3c00cdb88ff408d3021c3c09f09daeec402
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-11-21 12:40:46 -0300

    Update Program.cs

--- dev/Operadores/Program.cs
+++ dev/Operadores/Program.cs
@@ -4,8 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        //int x = 25;
-       //Console.WriteLine(x < 25);
-    
+        int idade = 18;
+        int maioridade = 21;
+        int idadeMaxima = 65;
+
+        if (idade >= maioridade)
+        {
+            Console.WriteLine("É diferente");
+            
+        }
+        else if(idade > 25)
+        {
+            System.Console.WriteLine("É igual");
+        }
+        else if(idade >= 32)
+        {
+            System.Console.WriteLine("É igual");
+        }
+        else if(idade >= 38)
+        {
+            System.Console.WriteLine("É igual");
+        }
+        else
+        {
+            System.Console.WriteLine("É igual");
+        }       
+
+        Console.WriteLine("Finalizou o programa");        
     }
 }
