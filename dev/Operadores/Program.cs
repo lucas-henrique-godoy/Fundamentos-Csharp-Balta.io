c1f12146077c4a16c55ffaf6ab84d0cf02c74230
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-11-21 15:09:44 -0300

    Update Program.cs

--- dev/Operadores/Program.cs
+++ dev/Operadores/Program.cs
@@ -4,13 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        string jogo = "God Of War";
+        /*string jogo = "God Of War";
         switch (jogo)
         {
             case "Call Of Duty": Console.WriteLine("Não é o jogo correto!"); break;
             case "Black": Console.WriteLine("Não é o jogo correto!"); break;
             case "God Of War": Console.WriteLine("É este!"); break;
             default: Console.WriteLine("Jogo não encontrado!"); break;
+        }
+       */
+
+
+        for (int i = 5; i >= 0; i--)
+        {
+            Console.WriteLine(i);
         }
         
     }        
