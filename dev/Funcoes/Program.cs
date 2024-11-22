70c9f9eae139b0668f93adf9b5528127ef07ecdc
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-11-22 10:58:19 -0300

    update Program

---
+++ dev/Funcoes/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Funcoes;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MeuMetodo();
+
+        string nome = RetornaNome("Lucas", "Henrique");
+        Console.WriteLine(nome);
+    }
+
+    static void MeuMetodo()
+    {
+        Console.WriteLine("C# é legal!");
+    }
+
+    static string RetornaNome(
+        string nome,
+        string sobrenome,
+        int idade;
+        )
+    {
+        return nome + " " + sobrenome;
+    }
+   
+}
