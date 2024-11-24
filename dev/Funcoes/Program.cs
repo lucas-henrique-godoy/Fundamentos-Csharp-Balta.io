e4cecb0a1fe148451ad14a59754dc6c394ad43d4
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-11-24 15:35:06 -0300

    Update Program.cs

--- dev/Funcoes/Program.cs
+++ dev/Funcoes/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Funcoes
+﻿using Internal;
+
+namespace Funcoes
 {
     class Program
     {
         static void Main(string[] args)
         {
            
+        }
+
+        static void MeuMetodo()
+        {
+            Console.WriteLine("C# é legal!");
         }       
               
     }
