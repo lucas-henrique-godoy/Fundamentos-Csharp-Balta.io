2de09c62c12a0fc75896b5dbe0b72ac940630634
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-11-25 16:02:43 -0300

    Criei o arquivo ValorReferencia para estudar sobre os tipos de valor e referência em C#.

---
+++ dev/ValorReferencia/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ValorReferencia;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /* Tipos de Valor
+        int x = 25;
+        int y = x;
+        Console.WriteLine(x);
+        Console.WriteLine(y);
+
+        x = 32;
+        Console.WriteLine(x);
+        Console.WriteLine(y);
+        */
+
+        var arr = new string[2];
+        arr[0] = "Item 1";
+
+        var arr2 = arr;
+    }
+}
