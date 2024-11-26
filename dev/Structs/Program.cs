9a8e12809c7253bdbaac315a220dce2ab1c2db13
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-11-26 11:15:19 -0300

    upgrade Program

--- dev/Structs/Program.cs
+++ dev/Structs/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Structs
 {
@@ -6,9 +7,36 @@ namespace Structs
     {
         static void Main(string[] args)
         {
-            
+            Product mouse = new Product(1, "Mouse Gamer", 299.97); // Instanciando um objeto da struct Product
+           
+            mouse.Id = 55; // Atribuindo um novo valor de id.
+
+            Console.WriteLine(mouse.Id);
+            Console.WriteLine(mouse.Name);
+            Console.WriteLine(mouse.Price);            
         }
    }
+
+   struct Product
+   {
+        public Product(int id, string name, double price) // Método Construtor
+        {
+            Id = id;          // Propriedade(maiúsculo) recebe parâmetro(minúsculo)
+            Name = name;     // Propriedade(maiúsculo) recebe parâmetro(minúsculo)
+            Price = price;  // Propriedade(maiúsculo) recebe parâmetro(minúsculo)
+        }
+        
+        public int Id; // Propriedade
+        public string Name; // Propriedade
+        public double Price; // Propriedade
+
+        public double PriceInDolar(double dolar) // Parâmetro
+        {
+            return Price * dolar;
+        }
+   }
+
+   
 }
 
 
