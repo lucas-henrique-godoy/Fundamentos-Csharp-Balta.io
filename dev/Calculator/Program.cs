7905f05ffb4f95fa4a7b453a1e6efd6ba65a22cd
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-11-26 12:51:37 -0300

    Update Program.cs

--- dev/Calculator/Program.cs
+++ dev/Calculator/Program.cs
@@ -8,7 +8,7 @@ namespace Calculator
         {
             Console.Clear();
             Console.WriteLine("Primeiro valor: ");
-            float v1 = float.Parse(Console.ReadLine());            
+            float v1 = float.Parse(Console.ReadLine());                      
 
             Console.WriteLine(v1);
         }
