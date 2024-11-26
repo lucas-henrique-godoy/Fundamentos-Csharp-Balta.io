636422fc5bf52a7f84efb9ddd950bd853c630a94
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-11-26 15:09:05 -0300

    Update Program.cs

--- dev/Calculator/Program.cs
+++ dev/Calculator/Program.cs
@@ -7,10 +7,18 @@ namespace Calculator
         static void Main(string[] args)
         {
             Console.Clear();
-            Console.WriteLine("Primeiro valor: ");
-            float v1 = float.Parse(Console.ReadLine());                      
 
-            Console.WriteLine(v1);
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");                     
+            
+            float resultado = v1 + v2;
+            Console.WriteLine("O resultado da soma é: " + resultado); // Concatenação
+            Console.WriteLine($"O resultado da soma é: {resultado}"); // Interpolação          
         }
     }
 }
