6c22425846eac0980e7c4d0ce05937cf20d566d0
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-11-26 15:13:42 -0300

    Update Program.cs

--- dev/Calculator/Program.cs
+++ dev/Calculator/Program.cs
@@ -6,7 +6,7 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.Clear();
+            Console.Clear(); // Limpa o Console
 
             Console.WriteLine("Primeiro valor: ");
             float v1 = float.Parse(Console.ReadLine());
@@ -17,9 +17,11 @@ namespace Calculator
             Console.WriteLine("");                     
             
             float resultado = v1 + v2;
-            Console.WriteLine("O resultado da soma é: " + resultado); // Concatenação
-            Console.WriteLine($"O resultado da soma é: {resultado}"); // Interpolação
-            Console.WriteLine($"O resultado da soma é: {v1 + v2}"); // Outro exemplo de Interpolação
+            Console.WriteLine("O resultado da soma é: " + resultado); // Concatenação.
+            Console.WriteLine("O resultado da soma é: " + (v1 + v2)); // Outro exemplo de Concatenação.
+            Console.WriteLine($"O resultado da soma é: {resultado}"); // Interpolação.
+            Console.WriteLine($"O resultado da soma é: {v1 + v2}"); // Outro exemplo de Interpolação.
+            
 
         }
     }
