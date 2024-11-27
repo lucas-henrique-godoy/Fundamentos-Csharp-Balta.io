8034d5907f28a852732562ef8f7824bbdcea36cc
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-11-27 14:41:40 -0300

    Criei e iniciei o projeto de Cronometro

---
+++ dev/Stopwatch/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        static void Start()
+        {
+            
+        }
+    }
+}
+
