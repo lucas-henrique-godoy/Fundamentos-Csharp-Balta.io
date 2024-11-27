c27b1a89717668f08290e8bfb0bd2210412dec2f
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-11-27 10:38:03 -0300

    Update Program.cs

--- dev/Calculator/Program.cs
+++ dev/Calculator/Program.cs
@@ -7,6 +7,8 @@ namespace Calculator
         static void Main(string[] args)
         {
             Soma();
+            Subtracao();
+            Multiplicacao();
         }
 
         static void Soma()
@@ -26,21 +28,46 @@ namespace Calculator
             //Console.WriteLine("O resultado da soma é: " + (v1 + v2)); // Outro exemplo de Concatenação.
             Console.WriteLine($"O resultado da soma é: {resultado}"); // Interpolação.
             //Console.WriteLine($"O resultado da soma é: {v1 + v2}"); // Outro exemplo de Interpolação.
+            Console.ReadKey();
         }
 
         static void Subtracao()
         {
+            Console.Clear();
 
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 - v2;
+            Console.WriteLine($"O resultado da subtração é: {resultado}");
+            Console.ReadKey();
         }
 
         static void Multiplicacao()
         {
+            Console.Clear();
 
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 * v2;
+            Console.WriteLine($"O resultado da multiplicação é: {resultado}");
+            Console.ReadKey();
         }
 
         static void Divisao()
         {
-            
+
         }
     }
 }
