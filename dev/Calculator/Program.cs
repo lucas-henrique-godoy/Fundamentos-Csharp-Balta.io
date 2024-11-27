09d3d9e2689a43bdcb91afe93fbf202f2bb68240
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-11-27 10:25:57 -0300

    Criando os métodos para as operações aritméticas da Calculadora

--- dev/Calculator/Program.cs
+++ dev/Calculator/Program.cs
@@ -6,6 +6,11 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            Soma();
+        }
+
+        static void Soma()
+        {
             Console.Clear(); // Limpa o Console
 
             Console.WriteLine("Primeiro valor: ");
@@ -17,12 +22,25 @@ namespace Calculator
             Console.WriteLine("");                     
             
             float resultado = v1 + v2;
-            Console.WriteLine("O resultado da soma é: " + resultado); // Concatenação.
-            Console.WriteLine("O resultado da soma é: " + (v1 + v2)); // Outro exemplo de Concatenação.
+            //Console.WriteLine("O resultado da soma é: " + resultado); // Concatenação.
+            //Console.WriteLine("O resultado da soma é: " + (v1 + v2)); // Outro exemplo de Concatenação.
             Console.WriteLine($"O resultado da soma é: {resultado}"); // Interpolação.
-            Console.WriteLine($"O resultado da soma é: {v1 + v2}"); // Outro exemplo de Interpolação.
-            
+            //Console.WriteLine($"O resultado da soma é: {v1 + v2}"); // Outro exemplo de Interpolação.
+        }
 
+        static void Subtracao()
+        {
+
+        }
+
+        static void Multiplicacao()
+        {
+
+        }
+
+        static void Divisao()
+        {
+            
         }
     }
 }
