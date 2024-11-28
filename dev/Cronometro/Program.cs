d4f1de141133ca161223918456a03e901108656f
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-11-28 12:24:32 -0300

    Criei o projeto de cronometro de novo

---
+++ dev/Cronometro/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace Cronomettro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = Segundo => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string dados = Console.ReadLine().ToLower();
+        }
+
+        static void Start(int time)
+        {
+            int tempoAtual = 0;
+
+            while (tempoAtual != time)
+            {
+                Console.Clear();                
+                tempoAtual++; 
+                Console.WriteLine(tempoAtual); 
+                Thread.Sleep(1000);                             
+            }
+
+            Console.Clear();
+            Console.WriteLine("Cronômetro finalizado");
+            Thread.Sleep(2500);
+        }
+    }
+}
+
