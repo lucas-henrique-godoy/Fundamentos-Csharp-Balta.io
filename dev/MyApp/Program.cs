3c56fd2cf032b167d903d7e581e51805367d4561
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-12-01 00:12:36 -0300

    Update Program.cs

--- dev/MyApp/Program.cs
+++ dev/MyApp/Program.cs
@@ -1,2 +1,13 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
