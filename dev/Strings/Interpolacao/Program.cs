491576f3b228a653d596b2884d9008cceb27b68d
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-12-02 11:59:24 -0300

    Update Program.cs

--- dev/Strings/Interpolacao/Program.cs
+++ dev/Strings/Interpolacao/Program.cs
@@ -7,8 +7,11 @@ namespace Interpolacao
         static void Main(string[] args)
         {
             var price = 10.2;
-
-            
+            //var texto = "O preço do produto é " + price + " apenas na promoção";
+            //var texto = string.Format("O preço do produto é {0} apenas na promoção", price);
+            var texto = $@"O preço do
+            produto é {price} apenas na promoção";
+            Console.WriteLine(texto);
         }
     }
 }
