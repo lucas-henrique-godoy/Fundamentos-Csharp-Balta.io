86b6c7d3b73a1d1067989a087575f04fbf30d4ae
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-12-01 22:44:43 -0300

    Update Program.cs

--- dev/MyApp/Program.cs
+++ dev/MyApp/Program.cs
@@ -7,6 +7,10 @@ namespace MyApp
         static void Main(string[] args)
         {
             var id = Guid.NewGuid();
+            id.ToString();
+
+            id = new Guid("798a3f69-9417-471f-9766-7ba247cc0a59");
+            id.ToString().Substring(0,8);
             Console.WriteLine(id);
         }
     }
