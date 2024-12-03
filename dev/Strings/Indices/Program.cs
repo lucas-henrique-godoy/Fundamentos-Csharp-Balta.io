03793dcec874201aa13db83322c37b7e07d71d85
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-12-03 11:33:32 -0300

    Update Program.cs

--- dev/Strings/Indices/Program.cs
+++ dev/Strings/Indices/Program.cs
@@ -1,14 +1,16 @@
-﻿using System;
+﻿using System; // Importa o namespace System, que contém classes essenciais como Console
 
-namespace Indices
+namespace Indices // Define o namespace "Indices" para organizar o código
 {
-    class Program
+    class Program // Define a classe Program
     {
-        static void Main()
+        static void Main() // Método principal, ponto de entrada da aplicação
         {
-            var texto = "Este texto é um teste";
-            Console.WriteLine(texto.IndexOf("é"));
-            Console.WriteLine(texto.LastIndexOf("s"));
+            var texto = "Este texto é um teste"; // Cria uma variável "texto" com o valor "Este texto é um teste"
+            
+            Console.WriteLine(texto.IndexOf("é")); // Procura a primeira ocorrência da substring "é" em "texto" e exibe o índice. O resultado será 15.
+            
+            Console.WriteLine(texto.LastIndexOf("s")); // Procura a última ocorrência da substring "s" em "texto" e exibe o índice. O resultado será 17.
         }
     }
 }
