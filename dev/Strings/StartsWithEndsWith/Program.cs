3349edc70fc149a4c737712fb8ef8fd27b7277bf
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-12-03 08:20:53 -0300

    Criei o Arquivo StartsWithEndsWith

---
+++ dev/Strings/StartsWithEndsWith/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StartsWithEndsWith;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var texto = "Este texto é um teste";
+        Console.WriteLine(texto.StartsWith("Este"));
+        Console.WriteLine(texto.StartsWith("este", StringComparison.OrdinalIgnoreCase));
+        Console.WriteLine(texto.StartsWith("texto"));
+    }
+}
