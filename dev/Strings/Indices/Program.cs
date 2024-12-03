570613aee01e43706ab3c4126d588589eb480a47
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-12-03 09:06:48 -0300

    Criei o  arquivo Indices

---
+++ dev/Strings/Indices/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Indices
+{
+    class Program
+    {
+        static void Main()
+        {
+            var texto = "Este texto é um teste";
+            Console.WriteLine(texto.IndexOf("é"));
+        }
+    }
+}
