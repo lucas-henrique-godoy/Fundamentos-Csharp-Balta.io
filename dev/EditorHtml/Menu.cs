7600b883df8624b0bc846db03dd44c076f9ef424
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-12-04 11:19:35 -0300

    upgrade Program

--- dev/EditorHtml/Menu.cs
+++ dev/EditorHtml/Menu.cs
@@ -6,7 +6,41 @@ namespace EditorHtml
     {
         public static void Show()
         {
-            
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Black; 
+
+            DrawScreen();           
+        }
+
+        public static void DrawScreen()
+        {
+            Console.Write("+");
+            for(int i = 0; i <= 30; i++)
+            {
+                Console.Write("-");
+            }
+            Console.Write("+");
+            Console.Write("\n");
+
+            for(int lines = 0; lines <= 10; lines++)
+            {
+                Console.Write("|");
+                for(int i = 0; i <= 30; i++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("|");
+                Console.Write("\n");
+            } 
+
+            Console.Write("+");
+            for(int i = 0; i <= 30; i++)
+            {
+                Console.Write("-");
+            }
+            Console.Write("+");
+            Console.Write("\n");           
         }
     }
 }
