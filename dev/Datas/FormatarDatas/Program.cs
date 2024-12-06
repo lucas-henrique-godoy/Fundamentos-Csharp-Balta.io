d2d581767d6a5ca0292a62660a94a0e15f169952
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-12-06 12:49:24 -0300

    Update Program.cs

--- dev/Datas/FormatarDatas/Program.cs
+++ dev/Datas/FormatarDatas/Program.cs
@@ -11,7 +11,7 @@ namespace FormatarDatas
             var data = DateTime.Now;
 
             
-            var formatada = String.Format("{0:yyyy * MM * dd}", data);// Formatando a saida de ano, mes e dia. Existem muitas formas diferentes de formatação.
+            var formatada = String.Format("{0:yyyy * MM * dd}", data); // Formatando a saida de ano, mes e dia. Existem muitas formas diferentes de formatação.
             Console.WriteLine(formatada);
         }
     }
