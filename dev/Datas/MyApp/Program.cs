6388855635a7c876bfc44c62ae764e4c5b3538b1
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-12-06 10:22:35 -0300

    Criei o diretório Datas, para estudar esse tipo de dado.

---
+++ dev/Datas/MyApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            
+            var data = new DateTime(2024, 12, 06, 10, 17, 00);
+            //var data = DateTime.Now; // Pega a data e hora atual
+            Console.WriteLine(data);
+            Console.WriteLine(data.Year);
+            Console.WriteLine(data.Month);
+            Console.WriteLine(data.Hour);
+            Console.WriteLine(data.Minute);
+            Console.WriteLine(data.Second);
+            Console.WriteLine(data.DayOfWeek); //Console.WriteLine((int)data.DayOfWeek) --> mostra o número correpondente ao dia da semana.
+        }
+    }
+}
+
+
