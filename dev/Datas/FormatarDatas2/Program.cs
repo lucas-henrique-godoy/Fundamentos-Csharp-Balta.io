4d5c10e709ea66085ccd82389f4ead01fb4282ed
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-12-07 21:47:14 -0300

    Update Program.cs

--- dev/Datas/FormatarDatas2/Program.cs
+++ dev/Datas/FormatarDatas2/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace FormatarDatas2
+﻿using System;
+using System.Datetime;
+
+namespace FormatarDatas2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Clear();
+
+            var data = Datetime.Now;
+
+            var formatada = string.Format("{0:t}", data)
+            Console.WriteLine(formatada);
         }
     }
 }
