8b4faeaff7221d6df532ef58dd84e7aecc2a5fef
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-12-08 22:03:23 -0300

    Update Program.cs

--- dev/Datas/AdicionarValoresDatas/Program.cs
+++ dev/Datas/AdicionarValoresDatas/Program.cs
@@ -6,11 +6,14 @@ namespace AdicionarValoresDatas
     {
         static void Main(string[] args)
         {
-            Console.Clear();
+            Console.Clear();  // Limpa a tela do console
 
-            var data = new DateTime.Now;
+            var data = DateTime.Now;  // Obtém a data e hora atuais e a armazena na variável 'data'
+            Console.WriteLine(data.AddDays(-12));  // Exibe a data atual, subtraindo 12 dias (data 12 dias atrás)
+            Console.WriteLine(data.AddMonths(1));  // Exibe a data atual, adicionando 1 mês
+            Console.WriteLine(data.AddYears(1));  // Exibe a data atual, adicionando 1 ano
+            Console.WriteLine(data.AddHours(1));  // Exibe a data e hora atuais, adicionando 1 hora
 
-            Console.WriteLine(data);
         }
     }
 }
