150e98cac6556113681bde9d13eb35dfb5e3a301
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-12-08 21:51:29 -0300

    Criei o AdicionarValoresDatas

---
+++ dev/Datas/AdicionarValoresDatas/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AdicionarValoresDatas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            var data = new DateTime.Now;
+
+            Console.WriteLine(data);
+        }
+    }
+}
+
+
