099ee2f2c5e43a609c768c536084c302b34f17e5
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-12-09 11:24:12 -0300

    Criei o Timezone

---
+++ dev/Datas/Timezone/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Timezone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            var utcDate = DateTime.UtcNow;
+            Console.WriteLine(dateTime);
+
+            Console.WriteLine(dateTime.ToLocalTime());
+        }
+    }
+}
+
+
