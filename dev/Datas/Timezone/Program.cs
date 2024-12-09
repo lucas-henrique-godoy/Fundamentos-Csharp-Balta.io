bc33cf54b8d476a157a85f9a4625c9fc6a5e893a
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-12-09 13:00:13 -0300

    Update Program.cs

--- dev/Datas/Timezone/Program.cs
+++ dev/Datas/Timezone/Program.cs
@@ -29,6 +29,15 @@ namespace Timezone
 
             // Exibe o horário de Auckland convertido no console.
             Console.WriteLine(horaAustralia);
+
+            var timezones = TimeZoneInfo.GetSystemTimeZones();
+            foreach (var timezone in timezones)
+            {
+                Console.WriteLine(timezone.Id);
+                Console.WriteLine(timezone);
+                Console.WriteLine(TimeZoneInfo.ConvertTimeFromUtc(utcDate, timezone));
+                Console.WriteLine("--------------");
+            }
         }
     }
 }
