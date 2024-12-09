58a40e145445c5a98e7fa271732dee9b37ecd011
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-12-08 22:09:52 -0300

    Criei o ComparacaoDatas

---
+++ dev/Datas/ComparacaoDatas/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ComparacaoDatas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            var data = DateTime.Now;
+
+            if (data == DateTime.Now)
+            {
+                Console.WriteLine("É igual");
+            }
+
+            Console.WriteLine(data);
+        }
+    }
+}
+
+
