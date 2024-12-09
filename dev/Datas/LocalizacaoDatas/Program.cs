b6a99f40fd50f00885dfe82f60c4d31e37713b94
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-12-09 10:03:45 -0300

    Update Program.cs

--- dev/Datas/LocalizacaoDatas/Program.cs
+++ dev/Datas/LocalizacaoDatas/Program.cs
@@ -9,9 +9,17 @@ namespace LocalizacaoDatas
         {
             Console.Clear();
 
-            var pt = new CultureInfo("pt-BR");
+            var pt = new CultureInfo("pt-PT");
+            var br = new CultureInfo("pt-BR");
+            var enUS = new CultureInfo("en-US");
+            var de = new CultureInfo("de-DE");
 
-            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(DateTime.Now.ToString("D", pt)); 
+            Console.WriteLine(DateTime.Now.ToString("D", br));         
+            Console.WriteLine(DateTime.Now.ToString("D", enUS));         
+            Console.WriteLine(DateTime.Now.ToString("D", de));                 
+            
+            
         }
     }
 }
