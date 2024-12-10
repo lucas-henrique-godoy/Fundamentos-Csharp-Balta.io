07b09f8980192fa02a26d0b0302015656bb5b78e
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-12-10 08:37:57 -0300

    Criei o diretório Moedas e dentro criei o Numbers para estudar sobre moedas em C#.

---
+++ dev/Moedas/Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            
+            var valor = 10.25;
+            
+            Console.WriteLine(valor);
+
+        }
+    }
+}
