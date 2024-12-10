6499eab23f3d38c31114894efc7c6207de8902c5
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-12-10 11:28:19 -0300

    Criei o EstudoMath para estudar métodos formatadores de valores e que fazem arredondamento. de decimais

---
+++ dev/Moedas/MathEstudo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MathEstudo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            decimal valor = 10536.25m;
+            Console.WriteLine(Math.Round(valor)); // Método que arredonda
+            Console.WriteLine(Math.Ceiling(valor)); // Método que arredonda para cima
+            Console.WriteLine(Math.Floor(valor)); // Método que arredonda para baixo
+        }
+    }   
+        
+}
+
