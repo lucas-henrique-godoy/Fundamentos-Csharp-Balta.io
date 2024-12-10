f0f75cf705860b15d33da3b044e6f3b392a1f286
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-12-10 12:42:22 -0300

    Criei o Arrays

---
+++ dev/Arrays/DotnetLists/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DotnetLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            var meuArray = new int[5]{ 23, 42, 54, 67, 99 };
+            meuArray[0] = 12;
+
+            Console.WriteLine(meuArray[0]); 
+            Console.WriteLine(meuArray[1]);
+            Console.WriteLine(meuArray[2]);
+            Console.WriteLine(meuArray[3]);           
+            Console.WriteLine(meuArray[4]);
+        }
+    }
+}
+
+
