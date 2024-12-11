2bc89f7b2616ea99cd5b2fffd5e3bb36ebbab381
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-12-11 12:44:01 -0300

    Upgrade ANOTACOES

--- dev/Arrays/AlterandoValores/Program.cs
+++ dev/Arrays/AlterandoValores/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace AlterandoValores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            var arr = new int[4];
+            var arrb = arr;
+
+            arr[0] = 23;
+            Console.WriteLine(arrb[0]);
+        }
+    }
+}
+
+
