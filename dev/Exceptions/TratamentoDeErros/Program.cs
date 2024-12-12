45154e7aea148037161d0374109ecf5e66f384ce
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-12-12 09:50:49 -0300

    Coloquei anotações sobre tratamento de exceções em C#

--- dev/Exceptions/TratamentoDeErros/Program.cs
+++ dev/Exceptions/TratamentoDeErros/Program.cs
@@ -7,6 +7,12 @@ namespace TratamentoDeErros
         static void Main(string[] args)
         {
            var arr = new int[3];
+
+           for(var index = 0; index < 10; index++)
+            {
+                // System.IndexOutOfRangeException:
+                Console.WriteLine(arr[index]);
+            }
         }
     }
 }
