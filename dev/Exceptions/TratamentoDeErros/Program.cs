8e086c12674cf24d437b3959f6265729d5b0fe7d
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-12-12 10:31:00 -0300

    Update Program.cs

--- dev/Exceptions/TratamentoDeErros/Program.cs
+++ dev/Exceptions/TratamentoDeErros/Program.cs
@@ -8,11 +8,20 @@ namespace TratamentoDeErros
         {
            var arr = new int[3];
 
-           for(var index = 0; index < 10; index++)
-            {
-                // System.IndexOutOfRangeException:
-                Console.WriteLine(arr[index]);
-            }
+           try
+           {
+                for(var index = 0; index < 10; index++)
+                {
+                    // System.IndexOutOfRangeException:
+                    Console.WriteLine(arr[index]);
+                }
+           }
+           catch(Exception ex) 
+           {
+                Console.WriteLine(ex.InnerException);
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Ops, algo deu errado!");
+           }           
         }
     }
 }
